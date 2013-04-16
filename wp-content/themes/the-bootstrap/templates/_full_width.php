eac7f12b1b5f5ae8ee8cf62f04188ff471e49d08
Clayton Phillipp <phillipp@Claytons-MacBook-Pro.local>
2013-04-16 17:34:01 -0600

    updated site

--- wp-content/themes/the-bootstrap/templates/_full_width.php
+++ wp-content/themes/the-bootstrap/templates/_full_width.php
@@ -9,21 +9,21 @@
  */
 
 get_header(); ?>
-
-<section id="primary" class="span12">
-	<?php tha_content_before(); ?>
-	<div id="content" role="main">
-		<?php tha_content_top();
-		
-		the_post();
-		get_template_part( '/partials/content', 'page' );
-		comments_template( '', true );
-		
-		tha_content_bottom(); ?>
-	</div><!-- #content -->
-	<?php tha_content_after(); ?>
-</section><!-- #primary -->
-
+<div class="custom-wrapper">
+	<section id="primary" class="span12">
+		<?php tha_content_before(); ?>
+		<div id="content" role="main">
+			<?php tha_content_top();
+			
+			the_post();
+			get_template_part( '/partials/content', 'page' );
+			comments_template( '', true );
+			
+			tha_content_bottom(); ?>
+		</div><!-- #content -->
+		<?php tha_content_after(); ?>
+	</section><!-- #primary -->
+</div>
 <?php
 get_footer();
 
