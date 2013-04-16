b7d11402630bcf646b541c58ddc2a67680ccc957
Clayton Phillipp <phillipp@Claytons-MacBook-Pro.local>
2013-04-16 16:56:27 -0600

    updated site

--- wp-content/themes/the-bootstrap/templates/home.php
+++ wp-content/themes/the-bootstrap/templates/home.php
@@ -6,19 +6,7 @@
 
 get_header(); ?>
 
-<section id="primary" class="span12">
-	<?php tha_content_before(); ?>
-	<div id="content" role="main">
-		<?php tha_content_top();
-		
-		the_post();
-		get_template_part( '/partials/content', 'page' );
-		comments_template( '', true );
-		
-		tha_content_bottom(); ?>
-	</div><!-- #content -->
-	<?php tha_content_after(); ?>
-</section><!-- #primary -->
+<?php if ( function_exists( 'meteor_slideshow' ) ) { meteor_slideshow(); } ?>
 
 <?php
 get_footer();
