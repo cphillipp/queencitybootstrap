100a989e900fd8b98196880bcfc5dc0bca6091d0
Clayton Phillipp <phillipp@Claytons-MacBook-Pro.local>
2013-04-17 15:03:46 -0600

    updated site

--- wp-content/themes/the-bootstrap/header.php
+++ wp-content/themes/the-bootstrap/header.php
@@ -30,13 +30,17 @@
 			});
 
 			window.onresize = function(event) {
-			resizeDiv();
+			var height = getDocHeight();
+			$('.background-pattern').css({'height': height + 'px'});
 			}
 
-			function resizeDiv() {
-			vpw = $(window).width(); 
-			vph = $(window).height(); 
-			$('.background-pattern').css({'height': vph + 'px'});
+			function getDocHeight() {
+			var D = document;
+			return Math.max(
+			Math.max(D.body.scrollHeight, D.documentElement.scrollHeight),
+			Math.max(D.body.offsetHeight, D.documentElement.offsetHeight),
+			Math.max(D.body.clientHeight, D.documentElement.clientHeight)
+			);
 			}
 		</script>
 		<div class="background-pattern">
